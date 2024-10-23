ac94e0ca8d868c2d22587d85939237a081140d41
guziqian <18701970151@163.com>
2024-10-23 18:46:43 +0800

    Update NumberFeature.java

--- src/main/java/org/example/NumberFeature.java
+++ src/main/java/org/example/NumberFeature.java
@@ -8,8 +8,18 @@ public class NumberFeature {
         int result = 0; // 输出结果
         Scanner in = new Scanner(System.in);
         inputNumber = in.nextInt();
+        int binaryValue = 1;
         // 你的代码逻辑
+        while (inputNumber > 0) {
+            int digit = inputNumber % 10; 
+            int position = (int) (Math.log10(inputNumber) + 1);  
+            inputNumber /= 10;
+            if ((digit % 2 == 1 && position % 2 == 1) || (digit % 2 == 0 && position % 2 == 0)) {
+                result += binaryValue;  
+            }
 
+            binaryValue *= 2;  
+        }
         System.out.println(result);
     }
 }
